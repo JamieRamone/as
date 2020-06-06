f2737041054586d3fd865b5758a3728eb5979676
JamieRamone <62776084+JamieRamone@users.noreply.github.com>
2020-06-05 23:43:21 -0300

    Add files via upload
    Added the copyright/licensing blurb to the make file and source code. Added the license file.

--- Makefile
+++ Makefile
@@ -1,3 +1,23 @@
+####################################################################################################################################
+#
+#	Makefile
+#
+#	This file is an part of Mondo SysUtils.
+#
+#	Copyright (C) 2020 Mondo Megagames.
+# 	Author: Jamie Ramone <sancombru@gmail.com>
+#	Date: 18-5-20
+#
+# This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+# published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with this program. If not, see
+# <http://www.gnu.org/licenses/>
+#
+####################################################################################################################################
 BINARY =		runas
 CFLAGS =		-fno-builtin-log
 CFLAGS +=		-fzero-initialized-in-bss
--- as.c
+++ as.c
@@ -1,3 +1,23 @@
+/***********************************************************************************************************************************
+ *
+ *	as.c
+ *
+ * This file is an part of Mondo SysUtils package.
+ *
+ *	Copyright (C) 2020 Mondo Megagames.
+ * 	Author: Jamie Ramone <sancombru@gmail.com>
+ *	Date: 18-5-20
+ *
+ * This program is free software: you can redistribute it and/or modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation, either version 3 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along with this program. If not, see
+ * <http://www.gnu.org/licenses/>
+ *
+ **********************************************************************************************************************************/
 #include <stdio.h>
 #include <stdbool.h>
 #include <stdlib.h>
